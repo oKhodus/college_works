7995745e8ea167a7eb90a41dd779790dd1ee652e
Oleksii Khodus <exalchel@gmail.com>
2025-03-16 01:17:59 +0200

    docs for task[dq]

--- DSA/pracs1/basic/dq.py
+++ DSA/pracs1/basic/dq.py
@@ -1,102 +1,108 @@
 class Deque:
     def __init__(self, capacity):
         """
-        _summary_
+        Initialize the deque with a fixed capacity
 
         Args:
-            capacity (int): _summary_
+            capacity (int): The maximum size of the deque
         """
-        self.capacity = capacity  # TODO: Initialize the deque with a fixed capacity
+        # Set the maximum capacity of the q
+        self.capacity = capacity  # TODO: Initialize the queue
+        # Create a ls with NONE to hold the elements of the q
         self.deque = [None] * capacity  # TODO: Create a fixed-size list
+        # front pointer starts at -1 - shows that the q is empty
         self.front = -1  # TODO: Set initial front pointer
+        # rear pointer starts at -1 - shows that the q is empty
         self.rear = -1  # TODO: Set initial rear pointer
 
     def is_empty(self):
-        """_summary_"""
-        # TODO: Check if the deque is empty
+        """Check if the queue is empty"""
+        # TODO: Check if the queue is empty
+        # The q is empty if front is -1
         return self.front == -1
 
     def is_full(self):
-        """_summary_"""
-        # TODO: Check if the deque is full
+        """Check if the queue is full"""
+        # TODO: Check if the queue is full
+        # The q is full when the next position after the rear == front position
         return (self.rear + 1) % self.capacity == self.front
 
     def insert_front(self, value):
-        """_summary_"""
-        # TODO: Insert an element at the front of the deque
+        """Insert an element at the front of the deque"""
         if self.is_full():
-            raise IndexError
+            raise IndexError("Oioioi, Queue is full")
 
         if self.is_empty():
+            # if the dq is empty, both front and rear are set to 0 (1st element)
             self.front = self.rear = 0
         else:
+            # move front pointer to the prev position
             self.front = (self.front - 1) % self.capacity
-
+        # insert value at the front of the dq
         self.deque[self.front] = value
-         
 
     def insert_rear(self, value):
-        """_summary_"""
-        # TODO: Insert an element at the rear of the deque
+        """Insert an element at the rear of the deque"""
         if self.is_full():
-            raise IndexError
+            raise IndexError("Oioioi, Queue is full")
 
         if self.is_empty():
+            # if the dq is empty - both front and rear are set to 0 (1st element)
             self.front = self.rear = 0
         else:
+            # move front pointer to the next position
             self.rear = (self.rear + 1) % self.capacity
-
+        # insert value at the rear of the dq
         self.deque[self.rear] = value
 
     def delete_front(self):
-        """_summary_"""
-        # TODO: Remove and return the front element
+        """Remove and return the front element"""
         if self.is_empty():
-            raise IndexError
-        
+            raise IndexError("Deque is empty, cannot delete from front")
+        # Get value at the front of the dq
         value = self.deque[self.front]
 
         if self.front == self.rear:
-            self.front = self.rear - 1
+            # if only 1 elem - reset both front and rear to -1 (empty dq)
+            self.front = self.rear = -1
         else:
+            # move front pointer to the next pos
             self.front = (self.front + 1) % self.capacity
 
         return value
 
     def delete_rear(self):
-        """_summary_"""
-        # TODO: Remove and return the rear element
+        """Remove and return the rear element"""
         if self.is_empty():
-            raise IndexError
-        
+            raise IndexError("Deque is empty, cannot delete from rear")
+        # Get value at the rear of the dq
         value = self.deque[self.rear]
 
         if self.front == self.rear:
-            self.front = self.rear - 1
+            # if only 1 elem  - reset both front and rear to -1 (empty dq)
+            self.front = self.rear = -1
         else:
-            self.rear = (self.rear + 1) % self.capacity
+            # move the rear pointer to the prev pos
+            self.rear = (self.rear - 1) % self.capacity
 
         return value
 
     def peek_front(self):
-        """_summary_"""
-        # TODO: Return the front element without removing it
+        """Return the front element without removing it"""
         if self.is_empty():
-            raise IndexError
-        
+            raise IndexError("Deque is empty, cannot peek from front")
+        # raeturn elem at the front without removing it
         return self.deque[self.front]
-    
+
     def peek_rear(self):
-        """_summary_"""
-        # TODO: Return the rear element without removing it
+        """Return the rear element without removing it."""
         if self.is_empty():
-            raise IndexError
-        
+            raise IndexError("Deque is empty, cannot peek from rear")
+        # return elem at the rear without removing it
         return self.deque[self.rear]
 
     def display(self):
-        """_summary_"""
-        # TODO: Display all elements in the deque
+        """Display all elements in the deque."""
         if self.is_empty():
             return
 
@@ -106,7 +112,7 @@ class Deque:
             all.append(self.deque[index])
             if index == self.rear:
                 break
-            index = (index + 1) % self.capacity
+            index = (index + 1) % self.capacity  # move to the next elem
         print(f"DQ: {all}")
 
 
@@ -117,9 +123,12 @@ dq.insert_rear(2)
 dq.insert_front(0)
 dq.display()
 
+"""Display the deque: [0, 1, 2]"""
+
 dq.delete_front()
 dq.display()
-
+"""Display the deque: [1, 2]"""
 dq.insert_front(-1)
 dq.insert_rear(3)
 dq.display()
+"""Display the deque: [-1, 1, 2, 3]"""
