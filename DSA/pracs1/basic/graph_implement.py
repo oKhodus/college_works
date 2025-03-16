3c0789b8d496c0a9975bc4f6b70f41bd0da293c3
Oleksii Khodus <exalchel@gmail.com>
2025-03-16 14:46:27 +0200

    docs for task [graph]

--- DSA/pracs1/basic/graph_implement.py
+++ DSA/pracs1/basic/graph_implement.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 
-
+"""abstract method - kind of template"""
 class GraphInterface(ABC):
     @abstractmethod
     def __init__(self, num_vertices: int) -> None:
@@ -29,32 +29,40 @@ class WeightedGraphInterface(GraphInterface):
 class Graph(GraphInterface):
     def __init__(self, num_vertices: int) -> None:
         self.nv = num_vertices
+        # adjancy ls (dict)
         self.adj_ls = {i: [] for i in range(num_vertices)}
 
     def add_edge(self, v1: int, v2: int) -> None:
+        """Add an undirected edge between v1 and v2"""
         self.adj_ls[v1].append(v2)
         self.adj_ls[v2].append(v1)
         return
 
     def is_adjacent(self, v1: int, v2: int) -> None:
+        """Check if v1 and v2 are adjacent"""
         return v2 in self.adj_ls[v1]
     
     def return_adjacent(self, v: int) -> list[int]:
+        """Return the list of adjacent vertices of v"""
         return self.adj_ls[v]
 
 class DirectedGraph(GraphInterface):
     def __init__(self, num_vertices: int) -> None:
         self.nv = num_vertices
+        # adjancy ls (dict)
         self.adj_ls = {i: [] for i in range(num_vertices)}
 
     def add_edge(self, v1: int, v2: int) -> None:
+        """Add a directed edge from v1 to v2"""
         self.adj_ls[v1].append(v2)
         return
     
     def is_adjacent(self, v1: int, v2: int) -> None:
+        """Check if there is a directed edge from v1 to v2"""
         return v2 in self.adj_ls[v1]
 
     def return_adjacent(self, v: int) -> list[int]:
+        """Return the list of adjacent vertices of v"""
         return self.adj_ls[v]
 
 
@@ -62,19 +70,25 @@ class DirectedGraph(GraphInterface):
 class WeightedGraph(WeightedGraphInterface):
     def __init__(self, num_vertices: int) -> None:
         self.num_vertices = num_vertices
+        # dict of dicts for weighted edges
         self.adj_ls = {i: {} for i in range(num_vertices)}
 
     def add_edge(self, v1: int, v2: int, w: int) -> None:
+        """Add a weighted edge between v1 and v2"""
         self.adj_ls[v1][v2] = w
+        # since it's an undirected weighted gh
         self.adj_ls[v2][v1] = w
 
     def is_adjacent(self, v1: int, v2: int) -> bool:
+        """Check if v1 and v2 are adjacent"""
         return v2 in self.adj_ls[v1]
 
     def return_adjacent(self, v: int) -> list[int]:
+        """Return the list of adjacent vertices of v"""
         return list(self.adj_ls[v].keys())
 
     def get_weight(self, v1: int, v2: int) -> int:
+        """Get the weight of the edge between v1 and v2"""
         return self.adj_ls[v1].get(v2, float('inf'))
 
 if __name__ == "__main__":
