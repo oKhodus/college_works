eb17a719acfbf51a4eebee5b78692e5096443197
oKhodus <exalchel@gmail.com>
2025-09-12 23:28:12 +0300

    do task 1.2

--- OOP/bankacc.py
+++ OOP/bankacc.py
@@ -1,12 +1,4 @@
-from datetime import datetime
-
 class BankAccount:
-    interest_rate = 0.005
-    counter = 0
-    # types = {
-    #     "D", "W", "I", "X"
-    # }
-
     def __init__(
         self,
         account_number: int,
@@ -18,43 +10,25 @@ class BankAccount:
         self.first_name = first_name
         self.last_name = last_name
         self.preferred_time_zone_offset = preferred_time_zone_offset
-
         self._balance = 0.0
 
-    @classmethod
-    def _next_tx_id(cls):
-        cls.counter += 1
-        return f"{cls.counter:03d}"
+    interest_rate = 0.005
 
     def apply_interest(self):
-        calc = self._balance * self.interest_rate
-        self._balance += calc
-        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        tx_id = self._next_tx_id()
-        print(f"I-{self.account_number}-{timestamp}-{tx_id}")
+        self._balance += (self._balance * self.interest_rate)
         return self._balance
 
     def get_balance(self):
         return self._balance
 
-    def deposit(self, inp):
-        if inp > 0:
-            self._balance += inp
-            timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-            tx_id = self._next_tx_id()
-            print(f"D-{self.account_number}-{timestamp}-{tx_id}")
+    def deposit(self, amount: float):
+        self._balance += amount
 
-    def withdraw(self, out):
-        if out <= self._balance:
-            self._balance -= out
-            timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-            tx_id = self._next_tx_id()
-            print(f"W-{self.account_number}-{timestamp}-{tx_id}")
-        else:
-            timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-            tx_id = self._next_tx_id()
-            print(f"X-{self.account_number}-{timestamp}-{tx_id}")
+    def withdraw(self, amount: float):
+        if amount > self._balance:
             print("Insufficient funds")
+            return
+        self._balance -= amount
 
 
 acc1 = BankAccount(140568, "Anna", "Smith", -7)
