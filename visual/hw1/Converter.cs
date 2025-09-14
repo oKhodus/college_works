bcdb3e11fd824e834f121ddc5bc27b61ae5487de
oKhodus <exalchel@gmail.com>
2025-09-14 19:47:28 +0300

    Do conv km to mile and backwards, refactor float issue

--- visual/hw1/Converter.cs
+++ visual/hw1/Converter.cs
@@ -8,8 +8,10 @@ class Converter
             @"Here is the list of converter options: 
     1. Convert from °C to °F 
     2. Convert from °F to °C
+    3. Convert from miles to kilometres
+    4. Convert from kilometres to miles
 
-Choose option from 1 to 2: ";
+Choose option from 1 to 4: ";
         Console.Write(invitation_text);
         int input_option = Convert.ToInt32(Console.ReadLine());
         switch (input_option)
@@ -18,15 +20,29 @@ Choose option from 1 to 2: ";
                 Console.Write("Enter value of temperature in Celsius: ");
                 string input_celsius = Console.ReadLine();
                 double celsius_converted = Convert.ToDouble(input_celsius);
-                double fahrenheit_calc = (celsius_converted * 9 / 5) + 32;
+                float fahrenheit_calc = (float)((celsius_converted * 9 / 5) + 32);
                 Console.WriteLine($"Temperature in Fahrenheit: {fahrenheit_calc}°");
                 break;
             case 2:
                 Console.Write("Enter value of temperature in Fahrenheit: ");
                 string input_fahr = Console.ReadLine();
                 double fahrenheit_converted = Convert.ToDouble(input_fahr);
-                double celsius_calc = (fahrenheit_converted - 32) * 5/9;
+                float celsius_calc = (float)((fahrenheit_converted - 32) * 5 / 9);
                 Console.WriteLine($"Temperature in Celsius: {celsius_calc}°");
+                break;
+            case 3:
+                Console.Write("Enter value of distance in miles: ");
+                string input_miles = Console.ReadLine();
+                double miles_converted = Convert.ToDouble(input_miles);
+                float km = (float)(miles_converted * 1.60934);
+                Console.WriteLine($"Distance in km: {km} kilometres");
+                break;
+            case 4:
+                Console.Write("Enter value of distance in kilometres: ");
+                string input_km = Console.ReadLine();
+                double km_converted = Convert.ToDouble(input_km);
+                float mile = (float)(km_converted * 0.621371);
+                Console.WriteLine($"Distance in miles: {mile} miles");
                 break;
         }
     }
@@ -35,3 +51,5 @@ Choose option from 1 to 2: ";
 
 // °F = (°C × 9/5) + 32 option 1.
 // °C = (°F - 32) × 5/9 option 2.
+// miles * 1.60934
+// km * 0.621371
