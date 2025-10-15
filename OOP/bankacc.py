1c5ebcfc626f86e6829671be71abd320b1501a10
Oleksii Khodus <exalchel@gmail.com>
2025-10-15 03:43:15 +0300

    Do task 1.4

--- OOP/bankacc.py
+++ OOP/bankacc.py
@@ -1,6 +1,11 @@
-from datetime import datetime
+from datetime import *
+from dataclasses import dataclass
+
 
 class BankAccount:
+    interest_rate = 0.005
+    _transaction_counter = 0
+
     def __init__(
         self,
         account_number: int,
@@ -14,45 +19,49 @@ class BankAccount:
         self.preferred_time_zone_offset = preferred_time_zone_offset
         self._balance = 0.0
 
-    interest_rate = 0.005
-    id = 0
+    def _next_transaction_id(self) -> str:
+        BankAccount._transaction_counter += 1
+        return "{:03d}".format(BankAccount._transaction_counter)
 
-    def apply_interest(self):
-        self._balance += (self._balance * self.interest_rate)
-        self.id += 1
-        current_time = datetime.today().strftime('%Y%m%d%H%M%S')
-        transaction_id = "{:03d}".format(self.id)
-        return f"I-{self.account_number}-{current_time}-{transaction_id}"
-        # return self._balance
+    def _make_code(self, type_code: str) -> str:
+        timestamp = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+        transaction_id = self._next_transaction_id()
+        return f"{type_code}-{self.account_number}-{timestamp}-{transaction_id}"
 
-    def get_balance(self):
+    def apply_interest(self) -> str:
+        self._balance += self._balance * self.interest_rate
+        return self._make_code("I")
+
+    def get_balance(self) -> float:
         return self._balance
 
-    def deposit(self, amount: float):
+    def deposit(self, amount: float) -> str:
         self._balance += amount
-        self.id += 1
-        current_time = datetime.today().strftime('%Y%m%d%H%M%S')
-        transaction_id = "{:03d}".format(self.id)
-        return f"D-{self.account_number}-{current_time}-{transaction_id}"
+        return self._make_code("D")
 
-    def withdraw(self, amount: float):
-        self.id += 1
-        current_time = datetime.today().strftime('%Y%m%d%H%M%S')
-        transaction_id = "{:03d}".format(self.id)
-
+    def withdraw(self, amount: float) -> str:
         if amount > self._balance:
             print("Insufficient funds")
-            return f"X-{self.account_number}-{current_time}-{transaction_id}"
-        
+            return self._make_code("X")
         self._balance -= amount
-        return f"W-{self.account_number}-{current_time}-{transaction_id}"
+        return self._make_code("W")
 
+    @staticmethod
+    def parse_confirmation(confirmation_code, timezone_offset):
+        code_arr = confirmation_code.split("-")
 
-acc1 = BankAccount(140568, "Anna", "Smith", -7)
-acc2 = BankAccount(222222, "Ben", "Lee", 2)
+        utc_now = datetime.now(timezone.utc)
+        current_time_utc = utc_now.strftime("%Y-%m-%dT%H:%M:%S")
 
-print(acc1.deposit(1000))
-print(acc1.withdraw(1100))
-print(acc1.apply_interest())
-print(acc1.withdraw(100))
-print(acc1.get_balance())
+        local_time = utc_now + timedelta(hours=timezone_offset)
+        time_local = local_time.strftime("%Y-%m-%d %H:%M:%S")
+
+        @dataclass
+        class ParseConf:
+            transaction_code: str = code_arr[0]
+            account_number: int = int(code_arr[1])
+            transaction_id: int = int(code_arr[3])
+            time_utc: str = current_time_utc
+            time: str = time_local
+
+        return ParseConf
